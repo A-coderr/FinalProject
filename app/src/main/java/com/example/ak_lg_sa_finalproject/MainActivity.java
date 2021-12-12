2a35e10e85bdab842ea7a46f95e83114eb16e617
anzhelika <akostyuk@conestogac.on.ca>
2021-12-11 21:25:23 -0500

    Added Services, Thread and Notification

--- app/src/main/java/com/example/ak_lg_sa_finalproject/MainActivity.java
+++ app/src/main/java/com/example/ak_lg_sa_finalproject/MainActivity.java
@@ -2,11 +2,21 @@ package com.example.ak_lg_sa_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Layout;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * This class is used to implement the home page of the application
@@ -14,6 +24,10 @@ import android.content.Intent;
  * @version 1.0
  */
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnChange;
+    private View mainView;
+    private Boolean changed = false;
 
     /**
      * initializing MainActivity
@@ -23,6 +37,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnChange = (Button) findViewById(R.id.btnChangeBackground);
+        mainView = (View) findViewById(R.id.constraintLayoutMain);
+
+        //Button for changing background color
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new ChangeBackgroundThread().execute("");
+            }
+        });
+
+        //Foreground service call
+        Intent serviceIntent = new Intent(this, ForegroundService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(serviceIntent);
+        }
+    }
+
+    /**
+     * this is Thread to change the background color of the main menu
+     */
+    public class ChangeBackgroundThread extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected void onPreExecute() {
+            Log.e("Background Thread", "Thread started");
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if(changed == false){
+                mainView.setBackgroundResource(R.drawable.background4);
+                changed = true;
+            }
+            else{
+                mainView.setBackgroundResource(R.drawable.background5);
+                changed = false;
+            }
+        }
     }
 
     /**
